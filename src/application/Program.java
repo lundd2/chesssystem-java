cebce2e8f4f434eff7904cef74d747bd8bf252e0
Lund Modolo <lundjpa@gmail.com>
2023-03-16 15:06:18 -0300

    Chess layer and ptinting the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,14 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+	
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-		
-		Board board = new Board(8, 8);
-			
 		}
 		
 	}
